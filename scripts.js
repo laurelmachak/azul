9b7a13d2b48b395de6484fb6aee5265f3307522a
Laurel Machak <laurelmachak@gmail.com>
2020-03-03 22:31:52 -0800

    need to fix major logic issues with placing tiles in staging

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 
+class Game_Controller {
+    constructor(){
+        this.tile_colors = ['A', 'B', 'C', 'D', 'E'];
+        this.tiles = [];
+    }
+}
+
 
 
 class Playing_Area {
@@ -28,13 +35,13 @@ class Row {
 
     init_staging() {
 
-        this.staging = new Staging_Row(this.max_number);
+        this.staging = new Staging_Row(this.row_number);
 
     }
 
 
     init_wall(){
-        this.wall = new Wall_Row(this.max_number);
+        this.wall = new Wall_Row(this.row_number);
     }
 
     available_wall_colors(){
@@ -45,8 +52,73 @@ class Row {
             }
         })
 
+        console.log(available_colors);
+        // this.staging.allowed_colors = available_colors;
         return available_colors;
     }
+
+    available_staging_colors(){
+        if (this.staging.is_empty()){
+            this.staging.allowed_colors = this.available_wall_colors();
+            return this.available_wall_colors();
+
+        } else if (this.staging.is_full()) {
+
+            this.staging.allowed_colors = [];
+            return [];
+
+        } else if (this.staging.has_tiles()){
+
+            this.staging.allowed_colors = this.staging.get_current_color();
+            return this.staging.get_current_color();
+        }
+    }
+
+    next_empty_staging_spot(){
+        
+        for (let i=0; i<this.staging.spots.length; i++){
+            if (this.staging.spots[i].placed_tile === null){
+                return this.staging.spots[i];
+            }
+        }
+    }
+
+    place_staging_tile(tile){
+
+        // if (this.staging.has_tiles()){
+        //     if (this.staging.is_full()){
+        //         return tile;
+        //     } else {
+        //         this.staging.get_current_color();
+        //     }
+        // } else {
+
+        // }
+
+        this.available_staging_colors();
+        if (this.staging.is_full()){
+            return tile;
+        } else if (this.staging.allowed_colors.includes(tile.color)){ //FIXX
+
+            let empty_space = this.next_empty_staging_spot();
+
+            empty_space.place_tile(tile);
+
+            // this.staging.spots.forEach(function(spot){
+            //     if (spot.placed_tile === null){
+            //         spot.place_tile(tile);
+            //         return;
+            //     } 
+            // })
+
+
+        } else {
+
+            return tile;
+
+        }
+    }
+
 }
 
 class Staging_Row {
@@ -70,6 +142,7 @@ class Staging_Row {
             }
         }
 
+        console.log(true);
         return true;
     }
 
@@ -83,7 +156,26 @@ class Staging_Row {
         return true;
     }
 
-    
+    has_tiles(){
+        for (let i=0; i<this.max_number; i++) {
+            if (this.spots[i].placed_tile != null){
+                return true;
+            } 
+        }
+
+        return false;
+    }
+
+    get_current_color(){
+        if (this.spots[0].placed_tile != null) {
+            this.allowed_colors = [this.spots[0].placed_tile.color];
+            console.log(this.allowed_colors);
+            return this.spots[0].placed_tile.color;
+        }
+    }
+
+
+
 
 }
 
@@ -121,6 +213,14 @@ class Wall_Row {
 
         this.spots.forEach(function(spot){
             if (spot.allowed_color === tile.color){
+
+                // JUST FOR TESTING :
+                if (!(spot.placed_tile === null)){
+                    console.log('already placed!');
+                    return tile;
+                }
+                // END TESTING IF
+
                 spot.placed_tile = tile;
                 console.log("placed: ", spot);
                 return true;
@@ -151,6 +251,10 @@ class Staging_Spot {
 
     }
 
+    place_tile(tile){
+        this.placed_tile = tile;
+    }
+
 
 }
 
@@ -168,6 +272,11 @@ class Tile {
 
 
 // TESTING:
+
+
+
+
+
 let colors = ['A', 'B', 'C', 'D', 'E'];
 let test_tiles = [];
 
@@ -177,9 +286,33 @@ colors.forEach(function(color){
     }
 })
 
-console.log(test_tiles);
 
-function randomly_populate_playing_area(area){
+
+function pick_random_tiles() {
+    let shuffled = test_tiles.sort(() => 0.5 - Math.random());
+
+    // Get sub-array of first n elements after shuffled
+    let selected = shuffled.slice(0, 8);
+
+    return selected;
+}
+
+
+function random_playing_area() {
+
+    let random_tiles = pick_random_tiles();
+
+    let area = new Playing_Area();
+
+
+    // place random tiles on random wall spaces
+    // Note: can only place valid tiles 
+    for (let i=0; i<random_tiles.length; i++){
+        let row = Math.ceil(Math.random() * 4);
+        area.rows[row].wall.place_tile(random_tiles[i]);
+    }
+
+    return area;
 
 
 }
@@ -198,31 +331,66 @@ function display_playing_area(area){
 }
 
 
-let test_playing_area = new Playing_Area();
-console.log('TEST PLAYING AREA: ',test_playing_area);
-let test_row = test_playing_area.rows[3];
-let test_staging_row = test_row.staging;
-let test_wall_row = test_row.wall;
+// let test_playing_area = new Playing_Area();
+// console.log('TEST PLAYING AREA: ',test_playing_area);
+// let test_row = test_playing_area.rows[3];
+// let test_staging_row = test_row.staging;
+// let test_wall_row = test_row.wall;
 
-console.log(test_playing_area.rows);
-console.log(test_staging_row);
-console.log(test_staging_row.is_empty());
-
-
-// console.log(test_playing_area.rows[3].spots.staging);
+// console.log(test_playing_area.rows);
+// console.log(test_staging_row);
+// console.log(test_staging_row.is_empty());
 
 
+// // console.log(test_playing_area.rows[3].spots.staging);
 
-console.log(test_wall_row.spots);
-console.log(test_wall_row.get_allowed_colors());
 
-test_wall_row.place_tile(test_tiles[40]);
 
-// display_playing_area(test_playing_area);
+// console.log(test_wall_row.spots);
+// console.log(test_wall_row.get_allowed_colors());
 
+// test_wall_row.place_tile(test_tiles[40]);
+// test_wall_row.place_tile(test_tiles[12]);
+// test_row.place_staging_tile(test_tiles[92]);
+// test_row.place_staging_tile(test_tiles[93]);
+
+
+//Playing Area
 let playing_table = document.getElementById('playing_area');
 let table_rows = playing_table.getElementsByClassName("row");
-console.log(table_rows);
+
+// Test Interactive tiles
+let tester_tile_color_picker = document.getElementById("testing_tiles");
+let testing_tiles = tester_tile_color_picker.getElementsByClassName('tile');
+
+let selected_color = 'A';
+
+let placing_tile = new Tile('A'); // Testing placing tile
+
+function select_tile(e){
+    for (let i=0; i<testing_tiles.length; i++){
+        testing_tiles[i].classList.remove('placed');
+    }
+
+
+    // darken selected
+    this.classList.add('placed');
+
+    selected_color = this.classList[1];
+
+    placing_tile.color = selected_color;
+    console.log(selected_color);
+    
+}
+
+for (let i=0; i<testing_tiles.length; i++){
+    testing_tiles[i].addEventListener('click', select_tile);
+}
+
+function place_on_staging(e){
+
+}
+
 
 function display_as_html(area){
     area.rows.forEach(function(row){
@@ -248,8 +416,26 @@ function display_as_html(area){
             
             let td = document.createElement('td');
             td.classList.add('staging_spot');
+            
+            if (staging_spot.placed_tile != null){
+                td.classList.add(staging_spot.placed_tile.color);
+                td.classList.add('placed');
+            }
+
+            td.addEventListener('click', function(){
+                row.place_staging_tile(placing_tile);
+                
+                if (staging_spot.placed_tile === placing_tile){
+                    td.classList.add(staging_spot.placed_tile.color);
+                    td.classList.add('placed');
+                }
+
+                
+            })
+
             // td.innerText = staging_spot.placed_tile;
             table_rows[html_tr_index].appendChild(td);
+
         })
 
 
@@ -260,4 +446,14 @@ function display_as_html(area){
 
 }
 
-display_as_html(test_playing_area);
+// display_as_html(test_playing_area);
+
+let test_area = random_playing_area();
+display_as_html(test_area);
+
+
+// Testing visual placing in staging
+
+
+
+
